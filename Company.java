51d58ab6c15c61f9e0f243d8379eea802dd5086a
MichalFlorianczyk <166236193+MichalFlorianczyk@users.noreply.github.com>
2024-04-07 19:13:01 +0200

    1

---
+++ Company.java
@@ -0,0 +1,35 @@
+import employees.Employee;
+import employees.Manager;
+import employees.Worker;
+
+public class Company {
+    public static void main(String[] args) {
+        // Przykładowa tablica pracowników
+        Employee[] employees = {
+                new Manager("Jan Kowalski", 5000.0), // indeks 0
+                new Worker("Anna Nowak", 3200.0, "Księgowa"),
+                new Employee("Zbigniew Twardowski", 3500.0),
+                new Employee("Karolina Kania", 4000.0),
+                new Worker("Michał Pazdan", 4500.0, "HR")
+        };
+
+        // Zliczenie nie-menadżerów
+        int nonManagersCount = 0;
+        for (Employee e : employees) {
+            if (!(e instanceof Manager)) {
+                nonManagersCount++;
+            }
+        }
+
+        // Ustawienie liczby podwładnych i pensji dla menadżera
+        if (employees[0] instanceof Manager) {
+            ((Manager) employees[0]).setNumberOfSubordinates(nonManagersCount);
+            employees[0].setSalary(7500.0);
+        }
+
+        // Wyświetlenie informacji o wszystkich pracownikach
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+    }
+}
